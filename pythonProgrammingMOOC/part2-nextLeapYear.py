b1a9fea4dcd80957cecd6b7c978579faf8c7a3d5
Scot Newbury <scot@scotnewbury.com>
2026-01-30 18:21:40 -0500

    Initial commit for the Next Leap Year coding challenge

---
+++ pythonProgrammingMOOC/part2-nextLeapYear.py
@@ -0,0 +1,17 @@
+# Part 2 - Simple Loops
+#
+# Please write a program which asks the user for a year, and prints out the next leap year.
+#
+# Sample output
+# Year: 2023
+# The next leap year after 2023 is 2024
+#
+# If the user inputs a year which is a leap year (such as 2024), the program should print out the following leap year:
+#
+# Sample output
+# Year: 2024
+# The next leap year after 2024 is 2028
+
+# The course has been focusing on simple loops and utilizing if/else statements while I could continue this
+# trend, I opted to accelerate things and start to include functions. In this case I'll be including a 
+#function to check if the year entered is a leap year.
