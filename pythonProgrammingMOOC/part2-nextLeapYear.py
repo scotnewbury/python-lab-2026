06887bca627d04b953327db62ac9fb6134abf2ea
Scot Newbury <scot@scotnewbury.com>
2026-01-30 19:23:30 -0500

    Completed solution with a function to test if a given year is a leap year

--- pythonProgrammingMOOC/part2-nextLeapYear.py
+++ pythonProgrammingMOOC/part2-nextLeapYear.py
@@ -16,8 +16,35 @@
 # trend, I opted to accelerate things and start to include functions. In this case I'll be including a 
 # function to check if the year entered is a leap year.
 
-def is_leap_year(year):
-  print(f"You passed the year, {year}")
 
+# Let's set up a function that returns true if a given year is a leap year
+# and false if it isn't
+#
+# year: integer
+# retun: boolean
+def is_leap_year(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        else:
+            return False
+    else:
+        return True
+  else:
+      return False
+
+# Get the year from the user, cast it as an integer
 year = int(input("Year: "))
-is_leap_year(year)
+
+# Let's calulate the next leap year
+# Take the modulo 4 of the year, subtract it from 4, add it to the year the user provided 
+nextLeapYear = year + (4 - (year % 4))
+
+# Did we calucate a vaild Leap Year?
+if is_leap_year(nextLeapYear):
+  # If we did
+  print(f"The next leap year after {year} is {nextLeapYear}")
+else:
+  # if we didn't, add four for the next Leap Year
+  print(f"The next leap year after {year} is {nextLeapYear+4}")
