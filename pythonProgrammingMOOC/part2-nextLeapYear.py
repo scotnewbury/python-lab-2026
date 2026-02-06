165b12979b38f375990f22cf7bcb2077dfad59f1
Scot Newbury <scot@scotnewbury.com>
2026-02-05 21:31:50 -0500

    flattened the code

--- pythonProgrammingMOOC/part2-nextLeapYear.py
+++ pythonProgrammingMOOC/part2-nextLeapYear.py
@@ -23,28 +23,34 @@
 # year: integer
 # retun: boolean
 def is_leap_year(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            return True
-        else:
-            return False
-    else:
-        return True
+  if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    return True
   else:
-      return False
+    return False
 
 # Get the year from the user, cast it as an integer
 year = int(input("Year: "))
+# Set our next leap year variable to the same year
+next_leap_year = year
 
 # Let's calulate the next leap year
 # Take the modulo 4 of the year, subtract it from 4, add it to the year the user provided 
-nextLeapYear = year + (4 - (year % 4))
+# nextLeapYear = year + (4 - (year % 4))
 
-# Did we calucate a vaild Leap Year?
-if is_leap_year(nextLeapYear):
-  # If we did
-  print(f"The next leap year after {year} is {nextLeapYear}")
-else:
-  # if we didn't, add four for the next Leap Year
-  print(f"The next leap year after {year} is {nextLeapYear+4}")
+# Check if the next_leap_year is a leap year if not, add one and retest
+while True:
+  if (is_leap_year(next_leap_year) and year != next_leap_year):
+    break
+  else:
+    next_leap_year += 1
+
+# Print the result
+print(f"The next leap year after {year} is {next_leap_year}")
+
+# # Did we calucate a vaild Leap Year?
+# if isLeapYear(nextLeapYear):
+#   # If we did
+#   print(f"The next leap year after {year} is {next_leap_year}")
+# else:
+#   # if we didn't, add four for the next Leap Year
+#   print(f"The next leap year after {year} is {next_leap_year+4}")
