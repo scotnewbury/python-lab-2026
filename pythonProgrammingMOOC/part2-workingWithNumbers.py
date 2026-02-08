578b31defda02b4298d02c29cf73abf3b1113ed5
Scot Newbury <scot@scotnewbury.com>
2026-02-07 22:53:23 -0500

    Altered main script logic to make exit criteria more prominent, also fixed small bug - tota1 instead of total

--- pythonProgrammingMOOC/part2-workingWithNumbers.py
+++ pythonProgrammingMOOC/part2-workingWithNumbers.py
@@ -18,15 +18,15 @@
 # ... the program asks for numbers
 # Numbers typed in 4
 
-# Part 2: Sum
-# The program should also print out the sum of all the numbers typed in. The zero at the end should not be included in the calculation.
+# Part 2: total
+# The program should also print out the total of all the numbers typed in. The zero at the end should not be included in the calculation.
 
 # The program should now print out the following:
 
 # Sample output
 # ... the program asks for numbers
 # Numbers typed in 4
-# The sum of the numbers is 34
+# The total of the numbers is 34
 
 # Part 3: Mean
 # The program should also print out the mean of the numbers. The zero at the end should not be included in the calculation. You may assume the user will always type in at least one valid non-zero number.
@@ -34,7 +34,7 @@
 # Sample output
 # ... the program asks for numbers
 # Numbers typed in 4
-# The sum of the numbers is 34
+# The tota1 of the numbers is 34
 # The mean of the numbers is 8.5
 
 # Part 4: Positives and negatives
@@ -43,7 +43,7 @@
 # Sample output
 # ... the program asks for numbers
 # Numbers typed in 4
-# The sum of the numbers is 34
+# The tota1 of the numbers is 34
 # The mean of the numbers is 8.5
 # Positive numbers 3
 # Negative numbers 1
@@ -55,25 +55,24 @@ print ("Please type in integer numbers. Type in 0 or just hit enter to finish.")
 number = 0
 number_count = 0
 positive = 0
-sum = 0
+total = 0
 
 while True:
   # Get an integer from the user, check for valid input
   number = get_integer_from_user("Number: ")
 
-  if number != 0:
+  if number == 0:
+    break
     number_count += 1
-    sum = sum + number
+    total = total + number
     if number > 0:
       positive += 1
-  else:
-    break
 
 print(f"Numbers typed in {number_count}")
-print(f"The sum of the numbers is {sum}")
+print(f"The total of the numbers is {total}")
 
 if number_count:
-  print(f"The mean of the numbers is {sum / number_count}")
+  print(f"The mean of the numbers is {total / number_count}")
 else:
   print("Cannot calculate mean, division by zero.")
 
