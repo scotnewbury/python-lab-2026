c7db93e7db5850731d356a3e9638d9c7552af8ca
Scot Newbury <scot@scotnewbury.com>
2026-02-14 22:03:44 -0500

    Couse solution finished for part 3 substrings part 2

--- pythonProgrammingMOOC/part3-substrings.py
+++ pythonProgrammingMOOC/part3-substrings.py
@@ -9,11 +9,30 @@
 # tes
 # test
 
+# user_string = input("Please type in a string: ")
+
+# string_index = 1
+
+# while string_index <= len(user_string):
+#   print(user_string[:string_index])
+#   string_index += 1
+
+# PART 2
+# Please write a program which asks the user to type in a string.
+# The program then prints out all the substrings which end with the last character,
+# from the shortest to the longest. Have a look at the example below.
+
+# Sample output
+# Please type in a string: test
+# t
+# st
+# est
+# test
+
 user_string = input("Please type in a string: ")
 
 string_index = 1
 
 while string_index <= len(user_string):
-  print(user_string[:string_index])
+  print(user_string[len(user_string)-string_index:])
   string_index += 1
-
