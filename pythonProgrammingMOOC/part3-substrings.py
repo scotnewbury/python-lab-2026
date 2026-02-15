7b8c7467d782ed2fdd0b8b3a81fef4ba95f5d98e
Scot Newbury <scot@scotnewbury.com>
2026-02-14 21:51:29 -0500

    Setup for substring challenges

---
+++ pythonProgrammingMOOC/part3-substrings.py
@@ -0,0 +1,10 @@
+# Please write a program which asks the user to type in a string. 
+# The program then prints out all the substrings which begin with the first character, 
+# from the shortest to the longest. Have a look at the example below.
+
+# Sample output
+# Please type in a string: test
+# t
+# te
+# tes
+# test
