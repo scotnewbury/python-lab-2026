e6c2117c811066cb2d4ef91b3266c9e889d8198a
Scot Newbury <scot@scotnewbury.com>
2026-02-14 22:17:02 -0500

    Started refactor of the contains vowel challenge

---
+++ pythonProgrammingMOOC/part3-contain_vowels_refactor.py
@@ -0,0 +1,37 @@
+# Please write a program which asks the user to input a string. 
+# The program then prints out different messages if the string contains any of the vowels a, e or o.
+
+# You may assume the input will be in lowercase entirely. Have a look at the examples below.
+
+# Sample output
+# Please type in a string: hello there
+# a not found
+# e found
+# o found
+
+# Sample output
+# Please type in a string: hiya
+# a found
+# e not found
+# o not found
+
+def main():
+  user_string = input("Please type in a string: ")
+
+  if "a" in user_string:
+    print ("a found")
+  else:
+    print ("a not found")
+
+  if "e" in user_string:
+    print ("e found")
+  else:
+    print ("e not found")
+
+  if "o" in user_string:
+    print ("o found")
+  else:
+    print ("o not found")
+
+if __name__ == "__main__":
+  main()
